19e2e5df04d95f36db429cbf73641a3235dbe85c
David Valero <valeroterrondavid@gmail.com>
2023-12-05 19:27:47 +0100

    🔩 07 Make SPA router WIP

--- projects/07-immdaviid-router/src/App.jsx
+++ projects/07-immdaviid-router/src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const NAVIGATION_EVENT = 'pushstate'
 
+function navigate (href) {
+  window.history.pushState({}, '', href)
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+  window.dispatchEvent(navigationEvent)
+}
+
+function HomePage () {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Home</h1>
+      <p>Esta es una página de ejemplo para crear un React Router desde 0</p>
+      <button onClick={() => navigate('/about')}>Ir a Sobre nosotros</button>
     </>
+  )
+}
+
+function AboutPage () {
+  return (
+    <>
+      <h1>About</h1>
+      <div>
+        <img src="https://pbs.twimg.com/profile_images/1726983029850038272/LKyDa6kb_400x400.jpg" alt="Fotografía de immdaviid" />
+        <p>Hola! Me llamo David y estoy creando un clon de React Router.</p>
+      </div>
+      <button onClick={() => navigate('/')}>Ir a la home</button>
+    </>
+  )
+}
+
+
+function App() {
+
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  }, [])
+
+  return (
+    <main>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
   )
 }
 
